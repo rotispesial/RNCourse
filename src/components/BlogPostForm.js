b95eebd6c468c4cce1677fb744609f79166d35a6
Kit Teguh <kit.teguh@gmail.com>
2020-06-15 23:57:07 +0800

    Lecture 148: - Refactored the crap out of the forms: created a new component and cut and paste everything relevant - Cut all return JSX statements, styles attribute, useState and useContext into BlogPostForm.js from CreateScreen - Same edits in EditScreen - Removed all redundant imported functions from refactored files

---
+++ src/components/BlogPostForm.js
@@ -0,0 +1,56 @@
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+
+const BlogPostForm = () => {
+
+    const [title, setTitle] = useState('');
+
+    const [content, setContent] = useState('');
+
+    return (
+        <View>
+            <Text style = {styles.labelStyle}>Enter title</Text>
+            <TextInput 
+                style = {styles.inputStyle}
+                value = {title} 
+                onChangeText = {(text) => setTitle(text)}   
+            />
+            <Text style = {styles.labelStyle}>Content Body: </Text>
+            <TextInput 
+                value = {content} 
+                onChangeText = {(text) => setContent(text)} 
+                style = {styles.inputStyle}
+            />
+            <Button 
+                title = "Save Post"
+            />
+        </View>
+    );
+
+};
+
+const styles = StyleSheet.create({
+        inputStyle:{
+            fontSize: 16,
+            borderWidth: 1,
+            borderColor: 'gray',
+            borderRadius: 5,
+            paddingHorizontal: 7,
+            paddingTop: 7,
+            marginHorizontal: 7,
+            marginVertical: 7,
+            backgroundColor: 'white'
+        },
+        labelStyle:{
+            fontWeight: 'bold',
+            fontSize: 20,
+            marginTop: 3,
+            marginLeft: 3
+        },
+        buttonStyle:{
+            fontWeight: 'bold',
+            fontSize: 26
+        }
+});
+
+export default BlogPostForm;
--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,29 +1,19 @@
-import React, { useState, useContext } from 'react';
-import { View, Text, StyleSheet, Button, TextInput } from 'react-native';
+import React, { useContext } from 'react';
+import { StyleSheet } from 'react-native';
 import { Context } from '../context/BlogContext';
+import BlogPostForm from '../components/BlogPostForm';
 
 const EditScreen = ({ navigation }) => {
 
-const { state } = useContext(Context);
+    const { state } = useContext(Context);
 
-//locate the ID to find which screen to edit
-const blogPost = state.find(
-    blogPost => blogPost.id === navigation.getParam('id')
-);
-
-const [title, setTitle] = useState(blogPost.title);
-
-const [content, setContent] = useState(blogPost.content);
-
-    return(
-        <View>
-            <Text>Edit Title:</Text>
-            <TextInput 
-                value = {title}
-                onChangeText = {(newTitle) => setTitle(newTitle)}
-            />
-        </View>
+    //locate the ID to find which screen to edit
+    const blogPost = state.find(
+        blogPost => blogPost.id === navigation.getParam('id')
     );
+
+    return <BlogPostForm />
+
 };
 
 const styles = StyleSheet.create({});
