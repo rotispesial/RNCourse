51e44b740aa01c7f216647270f3a1496b0f60119
Kit Teguh <kit.teguh@gmail.com>
2020-06-15 23:43:01 +0800

    Lectures 145-147: - Created Edit Screen - Hooked up navigation of edit screen in ShowScreen using navigationOptions to insert a title via headerRight TouchableOpacity - Set default blog post as initial state in BlogContext.js - Used useContext and useState to update the edits for the new content - Used state.find to cycle through the existing IDs to find the right one to edit - Used navigation.getParam('id') to match the ID that we want to edit - Set TextInput with onChangeText function to detect changes

--- App.js
+++ App.js
@@ -6,13 +6,15 @@ import IndexScreen from './src/screens/IndexScreen';
 import { Provider } from './src/context/BlogContext';
 import ShowScreen from './src/screens/ShowScreen';
 import CreateScreen from './src/screens/CreateScreen';
+import EditScreen from './src/screens/EditScreen';
 
 const navigator = createStackNavigator(
     {
     //List out all the screens users can navigate to
     Index: IndexScreen,
     Show: ShowScreen,
-    Create: CreateScreen
+    Create: CreateScreen,
+    Edit: EditScreen
 
 },{
     //2nd arg: Configuration specific to stack navigator, such as initial route
---
+++ src/screens/EditScreen.js
@@ -0,0 +1,31 @@
+import React, { useState, useContext } from 'react';
+import { View, Text, StyleSheet, Button, TextInput } from 'react-native';
+import { Context } from '../context/BlogContext';
+
+const EditScreen = ({ navigation }) => {
+
+const { state } = useContext(Context);
+
+//locate the ID to find which screen to edit
+const blogPost = state.find(
+    blogPost => blogPost.id === navigation.getParam('id')
+);
+
+const [title, setTitle] = useState(blogPost.title);
+
+const [content, setContent] = useState(blogPost.content);
+
+    return(
+        <View>
+            <Text>Edit Title:</Text>
+            <TextInput 
+                value = {title}
+                onChangeText = {(newTitle) => setTitle(newTitle)}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default EditScreen;
