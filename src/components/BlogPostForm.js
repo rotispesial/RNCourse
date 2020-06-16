60857f3a117333f664034d2298ab594cf648fe12
Kit Teguh <kit.teguh@gmail.com>
2020-06-16 09:00:03 +0800

    Lecture 149: - Customize onSubmit() by adding new param in BlogPostForm - Refactored CreateScreen to return <BlogPostForm> with onSubmit values

--- src/components/BlogPostForm.js
+++ src/components/BlogPostForm.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
-const BlogPostForm = () => {
+const BlogPostForm = ({ onSubmit }) => {
 
     const [title, setTitle] = useState('');
 
@@ -23,6 +23,7 @@ const BlogPostForm = () => {
             />
             <Button 
                 title = "Save Post"
+                onPress = {() => onSubmit(title, content)}
             />
         </View>
     );
--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,65 +1,20 @@
-import React, { useContext, useState } from 'react';
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import React, { useContext } from 'react';
+import { StyleSheet } from 'react-native';
 import { Context } from '../context/BlogContext';
+import BlogPostForm from '../components/BlogPostForm';
 
 const CreateScreen = ( {navigation} ) => {
 
-    const [title, setTitle] = useState('');
-
-    const [content, setContent] = useState('');
-
     const {addBlogPost} = useContext(Context);
+
+    return <BlogPostForm 
+        onSubmit = { (title, content) => {
+            addBlogPost(title, content, () => navigation.navigate('Index'))
+        } }
+    />
  
-    return (
-        <View>
-            <Text style = {styles.labelStyle}>Enter title</Text>
-            <TextInput 
-                style = {styles.inputStyle}
-                value = {title} 
-                onChangeText = {(text) => setTitle(text)}   
-            />
-            <Text style = {styles.labelStyle}>Content Body: </Text>
-            <TextInput 
-                value = {content} 
-                onChangeText = {(text) => setContent(text)} 
-                style = {styles.inputStyle}
-            />
-            <Button 
-                title="Save Blog Post"
-                style={styles.buttonStyle}
-                onPress = {() => {
-                    addBlogPost(title, content, () => {
-                        navigation.navigate('Index');
-                    });
-                }}
-            />
-        </View>
-    );
 };
 
-const styles = StyleSheet.create({
-    inputStyle:{
-        fontSize: 16,
-        borderWidth: 1,
-        borderColor: 'gray',
-        borderRadius: 5,
-        paddingHorizontal: 7,
-        paddingTop: 7,
-        marginHorizontal: 7,
-        marginVertical: 7,
-        backgroundColor: 'white'
-    },
-    labelStyle:{
-        fontWeight: 'bold',
-        fontSize: 20,
-        marginTop: 3,
-        marginLeft: 3
-    },
-    buttonStyle:{
-        fontWeight: 'bold',
-        fontSize: 26
-    }
-
-});
+const styles = StyleSheet.create({});
 
 export default CreateScreen;
